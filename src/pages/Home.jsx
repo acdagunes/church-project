98a61988a770dccb0981a0ed875765ade9f000ad
Church Project <church@example.com>
2026-02-05 19:20:45 +0400

    fix: removed aggressive dividers and implemented soft atmospheric blending

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -68,12 +68,6 @@ const Home = () => {
                     <span>↓</span>
                 </div>
 
-                {/* Transition Graphic */}
-                <div className="section-divider divider-bottom">
-                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                        <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5,73.84-4.36,147.54,16.88,218.2,35.26,69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113,1.11,1200,0H0Z" className="divider-fill-parchment"></path>
-                    </svg>
-                </div>
             </section>
 
             {/* About Preview */}
@@ -113,12 +107,8 @@ const Home = () => {
                 </video>
                 <div className="video-overlay"></div>
 
-                {/* Top Divider */}
-                <div className="section-divider divider-top" style={{ transform: 'rotate(180deg)' }}>
-                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                        <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5,73.84-4.36,147.54,16.88,218.2,35.26,69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113,1.11,1200,0H0Z" className="divider-fill-parchment"></path>
-                    </svg>
-                </div>
+                {/* Cinematic Fade for Video */}
+                <div className="cinematic-fade-bottom"></div>
 
                 <div className="container foundation-content">
                     <div className="content-box fade-in">
@@ -132,12 +122,6 @@ const Home = () => {
                     </div>
                 </div>
 
-                {/* Bottom Divider */}
-                <div className="section-divider divider-bottom">
-                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                        <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5,73.84-4.36,147.54,16.88,218.2,35.26,69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113,1.11,1200,0H0Z" className="divider-fill-dark"></path>
-                    </svg>
-                </div>
             </section>
 
 
